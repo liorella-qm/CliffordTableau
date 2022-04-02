798a2ff85f4989f2982d8266ccba0f3afa98d409
Lior Ella <lior@quantum-machines.co>
2022-04-02 20:52:47 +0300

    composition works logically but symplectic test fails

--- simple_tableau.py
+++ simple_tableau.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple, List
+from functools import reduce
+from typing import Dict, Tuple, List, Union
 
 import numpy as np
 from numpy.typing import ArrayLike
@@ -15,7 +16,7 @@ def g(x0z0, x1z1):
         return x1z1[1] - x1z1[0]
 
 
-def beta(v, u):
+def _beta(v, u):
     u = np.array(u)
     v = np.array(v)
     uz = u[1::2]
@@ -26,59 +27,42 @@ def beta(v, u):
     return (np.dot(vz + uz, vx + ux) % 2 - np.dot(vz, vx) % 2 - np.dot(uz, ux) % 2 + 2 * (np.dot(vx, uz) % 2)) % 4
 
 
-def compose_alpha(g1, alpha1, g2, alpha2):
+def _compose_alpha(g1, alpha1, g2, alpha2):
     n = len(alpha1) // 2
     alpha21 = []
     two_alpha21 = 2 * alpha1 % 4
     for i in range(2 * n):
         a = g1[:, i]
-        #         print('a=', a)
         for j in range(0, 2 * n, 2):
             ax = a[j]
-            az = a[j+1]
-            two_alpha21[i] += 2 * ax * alpha2[j] + 2 * az * alpha2[j+1] % 4
+            az = a[j + 1]
+            two_alpha21[i] += 2 * ax * alpha2[j] + 2 * az * alpha2[j + 1] % 4
             if ax * az:
-                #                 print('g2x=', g2[:, j])
-                #                 print('g2z=', g2[:, j+1])
-                #                 print('beta=', beta(g2[:, j], g2[:, j+1]))
-                s = (beta(g2[:, j], g2[:, j + 1]) + 1) % 4
+                s = (_beta(g2[:, j], g2[:, j + 1]) + 1) % 4
                 assert s % 2 == 0, 'expression does not divide by 2'
                 two_alpha21[i] += s % 4
         assert two_alpha21[i] % 2 == 0
-        alpha21.append(two_alpha21[i] // 2 % 2)  # not sure
+        alpha21.append(two_alpha21[i] // 2 % 2)
     return np.array(alpha21).astype(np.uint8)
 
 
 class SimpleTableau:
     def __init__(self, g, alpha):
+        g = np.array(g, dtype=np.uint8)
+        alpha = np.array(alpha, dtype=np.uint8)
         if not len(alpha) % 2 == 0:
             raise ValueError(f'alpha needs to have an even length but length is {len(alpha)}')
+        g_shape = g.shape
+        if not (len(g_shape) == 2 and g_shape[0] == g_shape[1] and g_shape[0] % 2 == 0):
+            raise ValueError(f'g has shape {g_shape}, which is not an even square matrix')
+        if not len(alpha) % 2 == 0:
+            raise ValueError(f'alpha has len {len(alpha)}, which is not even')
         self._n = len(alpha) // 2
+        # if not _is_symplectic(g, self._n):
+        #     raise ValueError('g is not a symplectic matrix')
         self._np_repr = np.vstack((
             g, alpha
         )).astype(np.uint8)
-
-    #         self._np_repr = np.vstack((np.identity(2 * n, dtype=np.uint8),
-    #                                   np.zeros((1, 2 * n), dtype=np.uint8)
-    #                                   )).astype(np.uint8)
-
-    def set_x_image(self, qubit, image):
-        """
-        set the image of x as a vector with 2*n+1 binary entries, (x0, z0, x1, z1, ..., sign)
-        """
-        raise NotImplementedError()
-
-    def set_z_image(self, qubit, image):
-        """
-        set the image of z as a vector with 2*n+1 binary entries, (x0, z0, x1, z1, ..., sign)
-        """
-        raise NotImplementedError()
-
-    def set_g(self, mat):
-        self._np_repr[:-1, :] = np.array(mat).astype(np.uint8)
-
-    def set_alpha(self, vec):
-        self._np_repr[-1] = np.array(vec).astype(np.uint8)
 
     @property
     def g(self):
@@ -87,6 +71,10 @@ class SimpleTableau:
     @property
     def alpha(self):
         return self._np_repr[-1]
+
+    @property
+    def n(self):
+        return self._n
 
     def __str__(self):
         # todo: work with more than single digit qubit number
@@ -104,11 +92,12 @@ class SimpleTableau:
     def __eq__(self, other):
         return np.array_equal(self._np_repr, other._np_repr)
 
-
-def compose(t1: SimpleTableau, t2: SimpleTableau) -> SimpleTableau:
-    g12 = t2.g @ t1.g % 2
-    alpha12 = compose_alpha(t1.g, t1.alpha, t2.g, t2.alpha)
-    return SimpleTableau(g12, alpha12)
+    def then(self, other: 'SimpleTableau') -> 'SimpleTableau':
+        if self.n != other.n:
+            raise ValueError(f'number of qubits of self={self.n} and of other={other.n} is incompatible')
+        g12 = other.g @ self.g % 2
+        alpha12 = _compose_alpha(self.g, self.alpha, other.g, other.alpha)
+        return SimpleTableau(g12, alpha12)
 
 
 _single_qubit_gate_conversions = {
@@ -121,11 +110,90 @@ _single_qubit_gate_conversions = {
     'sx': (np.array([[1, 1], [0, 1]]), np.array([0, 1])),
     'sy': (np.array([[0, 1], [1, 0]]), np.array([1, 0])),
     'msy': (np.array([[0, 1], [1, 0]]), np.array([0, 1]))
-
 }
 
+_two_qubit_gate_conversions = {
+    'cnot': (np.array([[1, 0, 1, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 1, 0, 1]]).T,
+             np.zeros(4)),
+    'iswap': (np.array([[0, 1, 1, 1],
+                        [1, 1, 0, 1],
+                        [0, 0, 0, 1],
+                        [0, 0, 1, 0]]).T,
+              np.zeros(4)),  # not 100% sure this is correct
+    'swap': (np.array([[0, 0, 1, 0],
+                       [0, 0, 0, 1],
+                       [1, 0, 0, 0],
+                       [0, 1, 0, 0]]).T,
+             np.zeros(4)),
+    'cz': (np.array([[1, 0, 0, 1],
+                     [0, 1, 0, 0],
+                     [0, 1, 1, 0],
+                     [0, 0, 0, 1]]).T,
+           np.zeros(4))
+}
 
-def generate_from_names(names: List[str]):
-    if len(names) > 1:
-        raise NotImplementedError('currently only single qubits supported')
-    return SimpleTableau(*_single_qubit_gate_conversions[names[0]])
+known_names = set(_single_qubit_gate_conversions.keys()).union(set(_two_qubit_gate_conversions.keys()))
+
+
+def generate_from_name(name: str, target: Union[int, Tuple[int, int]], n=None) -> SimpleTableau:
+    """
+    Generate a `SimpleTableau` object from a name.
+    Args:
+        name: gate name, must be in `known_names`
+        target: For a single qubit gate, the qubit on which the gate operates, from 0 to n-1. For two qubit gates,
+                a tuple of the form (control, target) on which the gate operates
+        n: number of total qubits on which gate operates
+
+    Returns: the `SimpleTableau` object
+
+    """
+    if n is None:
+        n = np.max(target) + 1
+    g = np.identity(2 * n)
+    alpha = np.zeros(2 * n)
+    if name in _single_qubit_gate_conversions:
+        if not isinstance(target, int) or target >= n:
+            raise ValueError(f'invalid target {target} for single qubit gate {name} on {n} qubits')
+        _embed_single_qubit_gate(alpha, g, name, target)
+        return SimpleTableau(g, alpha)
+
+    elif name in _two_qubit_gate_conversions:
+        if not isinstance(target, tuple) or max(target) >= n:
+            raise ValueError(f'invalid target {target} for two qubit gate {name} on {n} qubits')
+        _embed_two_qubit_gate(alpha, g, name, target)
+        return SimpleTableau(g, alpha)
+    else:
+        raise ValueError(f'unknown gate {name}')
+
+
+def _embed_two_qubit_gate(alpha, g, name, target):
+    g2q, alpha2q = _two_qubit_gate_conversions[name]
+    g[2 * target[0]: 2 * target[0] + 2, 2 * target[0]: 2 * target[0] + 2] = g2q[:2, :2]
+    g[2 * target[0]: 2 * target[0] + 2, 2 * target[1]: 2 * target[1] + 2] = g2q[:2, 2:4]
+    g[2 * target[1]: 2 * target[1] + 2, 2 * target[0]: 2 * target[0] + 2] = g2q[2:4, :2]
+    g[2 * target[1]: 2 * target[1] + 2, 2 * target[1]: 2 * target[1] + 2] = g2q[2:4, 2:4]
+    alpha[2 * target[0]: 2 * target[0] + 2] = alpha2q[:2]
+    alpha[2 * target[1]: 2 * target[1] + 2] = alpha2q[2:4]
+
+
+def _embed_single_qubit_gate(alpha, g, name, target):
+    g[2 * target:2 * target + 2, 2 * target:2 * target + 2] = _single_qubit_gate_conversions[name][0]
+    alpha[2 * target:2 * target + 2] = _single_qubit_gate_conversions[name][1]
+
+
+def _lambda(n):
+    """
+    based on definition here https://arxiv.org/abs/1406.2170
+    """
+    return np.block([
+        [np.zeros((n, n)), np.identity(n)],
+        [np.identity(n), np.zeros((n, n))]
+    ]).astype(np.uint8)
+
+
+def _is_symplectic(mat, n):
+    lhs = mat @ _lambda(n) @ mat.T % 2
+    return np.all(lhs == _lambda(n))
--- test_simple_tableau.py
+++ test_simple_tableau.py
@@ -1,10 +1,6 @@
-from pprint import pprint
-
-import cirq
 import numpy as np
 
-from clifford_tableau import CliffordTableau
-from simple_tableau import SimpleTableau, generate_from_names, compose, beta
+from simple_tableau import SimpleTableau, _beta, generate_from_name
 
 
 def test_beta():
@@ -19,11 +15,8 @@ def test_beta():
     for i in range(4):
         res.append([])
         for j in range(4):
-            print((dd[i], dd[j], beta(dd[i], dd[j])))
-            res[i].append(beta(dd[i], dd[j]))
+            res[i].append(_beta(dd[i], dd[j]))
             assert res[i][j] == expectation[i][j]
-
-    # assert beta([1, 0], [0, 1]) == 3
 
 
 def test_creation():
@@ -32,38 +25,121 @@ def test_creation():
     # todo: assert
 
 
-def test_from_name():
-    s = generate_from_names(['s'])
-    print(s)
-    # todo: assert
-
-
 def test_eq():
-    id1 = generate_from_names(['id'])
-    id2 = generate_from_names(['id'])
-    s = generate_from_names(['s'])
+    id1 = generate_from_name('id', 0)
+    id2 = generate_from_name('id', 0)
+    s = generate_from_name('s', 0)
     assert id1 == id2
     assert id1 != s
 
 
-def test_compose():
-    i2 = generate_from_names(['id'])
-    s = generate_from_names(['s'])
-    z = generate_from_names(['z'])
-    h = generate_from_names(['h'])
-    sx = generate_from_names(['sx'])
-    x = generate_from_names(['x'])
-    sy = generate_from_names(['sy'])
-    msy = generate_from_names(['msy'])
-    y = generate_from_names(['y'])
-    assert compose(h, h) == i2
-    assert compose(s, s) == z
-    assert compose(sx, sx) == x
-    assert compose(sy, sy) == y
-    assert compose(s, h) == SimpleTableau(np.array([[1, 1], [1, 0]]).T, [1, 0])
-    assert compose(x, y) == z
-    assert compose(y, x) == z
-    assert compose(sy, msy) == i2
-    assert compose(msy, sy) == i2
-    assert compose(sy, x) == h
-    assert compose(x, msy) == h
+def test_from_name_single_qubit():
+    s = generate_from_name('s', 0)
+    print(s)
+    assert s.g.shape == (2, 2)
+    assert len(s.alpha) == 2
+    assert np.array_equal(s.g, np.array([[1, 0], [1, 1]], dtype=np.uint8))
+    assert np.array_equal(s.alpha, np.zeros_like(s.alpha))
+
+
+def test_from_name_single_qubit_on_2_qubit_operator():
+    s = generate_from_name('s', 0, n=2)
+    assert s.g.shape == (4, 4)
+    assert len(s.alpha) == 4
+    assert np.array_equal(s.g, np.array([[1, 0, 0, 0],
+                                         [1, 1, 0, 0],
+                                         [0, 0, 1, 0],
+                                         [0, 0, 0, 1]], dtype=np.uint8))
+    assert np.array_equal(s.alpha, np.zeros_like(s.alpha))
+
+    s = generate_from_name('s', 1, n=2)
+    assert s.g.shape == (4, 4)
+    assert len(s.alpha) == 4
+    assert np.array_equal(s.g, np.array([[1, 0, 0, 0],
+                                         [0, 1, 0, 0],
+                                         [0, 0, 1, 0],
+                                         [0, 0, 1, 1]], dtype=np.uint8))
+    assert np.array_equal(s.alpha, np.zeros_like(s.alpha))
+    y1 = generate_from_name('y', 1, n=2)
+
+
+def test_from_name_two_qubit():
+    gate = generate_from_name('cnot', (0, 1))
+    assert gate == SimpleTableau(np.array([[1, 0, 1, 0],
+                                           [0, 1, 0, 0],
+                                           [0, 0, 1, 0],
+                                           [0, 1, 0, 1]]).T,
+                                 np.zeros(4))
+    # test that flipping control and target works
+    gate = generate_from_name('cnot', (1, 0))
+    assert gate == SimpleTableau(np.array([[1, 0, 0, 0],
+                                           [0, 1, 0, 1],
+                                           [1, 0, 1, 0],
+                                           [0, 0, 0, 1]]).T,
+                                 np.zeros(4))
+    generate_from_name('swap', (0, 1))
+    generate_from_name('swap', (1, 0))
+    # generate_from_name('iswap', (1, 0))
+    # generate_from_name('iswap', (0, 1))
+    generate_from_name('cz', (0, 1))
+    generate_from_name('cz', (1, 0))
+
+
+def test_compose_single_qubit():
+    i2 = generate_from_name('id', 0)
+    s = generate_from_name('s', 0)
+    z = generate_from_name('z', 0)
+    h = generate_from_name('h', 0)
+    sx = generate_from_name('sx', 0)
+    x = generate_from_name('x', 0)
+    sy = generate_from_name('sy', 0)
+    msy = generate_from_name('msy', 0)
+    y = generate_from_name('y', 0)
+    y0 = generate_from_name('y', 0, n=2)
+    y1 = generate_from_name('y', 1, n=2)
+    assert h.then(h) == i2
+    assert s.then(s) == z
+    assert sx.then(sx) == x
+    assert sy.then(sy) == y
+    assert s.then(h) == SimpleTableau(np.array([[1, 1], [1, 0]]).T, [1, 0])
+    assert x.then(y) == z
+    assert y.then(x) == z
+    assert sy.then(msy) == i2
+    assert msy.then(sy) == i2
+    assert sy.then(x) == h
+    assert x.then(msy) == h
+
+    assert y1.then(y0) == y0.then(y1)
+    assert y1.then(y0) == SimpleTableau(np.identity(4), [1, 1, 1, 1])
+
+
+def test_compose_two_qubit():
+    # flipping control and target with hadamards
+    h0 = generate_from_name('h', 0, 2)
+    h1 = generate_from_name('h', 1, 2)
+    cnot_ab = generate_from_name('cnot', (0, 1))
+    cnot_ba = generate_from_name('cnot', (1, 0))
+    assert h0.then(h1).then(cnot_ab).then(h0).then(h1) == cnot_ba
+
+    # cnot commutation identities
+    x0 = generate_from_name('x', 0, 2)
+    x1 = generate_from_name('x', 1, 2)
+    z0 = generate_from_name('z', 0, 2)
+    z1 = generate_from_name('z', 1, 2)
+    assert x0.then(cnot_ab) == cnot_ab.then(x0).then(x1)
+    assert z0.then(cnot_ab) == cnot_ab.then(z0)
+    assert z1.then(cnot_ab) == cnot_ab.then(z0).then(z1)
+    assert x1.then(cnot_ab) == cnot_ab.then(x1)
+
+    # swap identity
+    swap = generate_from_name('swap', (0, 1), 2)
+    swap_reverse = generate_from_name('swap', (1, 0), 2)
+    assert swap_reverse == swap
+    assert cnot_ab.then(cnot_ba).then(cnot_ab) == swap
+
+    # cz identities
+    cz = generate_from_name('cz', (0, 1), 2)
+    cz_reverse = generate_from_name('cz', (1, 0), 2)
+    assert cz == cz_reverse
+    assert h1.then(cnot_ab).then(h1) == cz
+    assert h0.then(cnot_ba).then(h0) == cz
